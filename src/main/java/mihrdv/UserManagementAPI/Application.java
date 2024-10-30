f85634fd1d39a724419ddfd2fe42ae0353b54926
Mihail <mihrdv@gmail.com>
2024-10-30 09:46:35 +0200

    Added service

--- src/main/java/mihrdv/UserManagementAPI/Application.java
+++ src/main/java/mihrdv/UserManagementAPI/Application.java
@@ -10,11 +10,4 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	private UserRepository userRepository;
-
-	public User createUser(User user){
-		return  userRepository.save(user);
-	}
-
-	
 }
