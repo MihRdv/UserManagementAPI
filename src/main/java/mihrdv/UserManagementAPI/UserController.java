38f6338d417e9b294f36521a046fb1d4879a50a0
Mihail <mihrdv@gmail.com>
2024-10-30 10:22:46 +0200

    Added controller too

---
+++ src/main/java/mihrdv/UserManagementAPI/UserController.java
@@ -0,0 +1,31 @@
+package mihrdv.UserManagementAPI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserById(Long id){
+        userService.deleteUserByID(id);
+    }
+
+
+}
